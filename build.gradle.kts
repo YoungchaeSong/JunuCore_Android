c4061410df166d1bef42d66cc58bf958b287593f
Youngchai Song <thddudco@gmail.com>
2025-06-05 15:55:28 +0900

    설정중

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.library") version "8.10.1"
-    id("org.jetbrains.kotlin.android") version "2.0.21"
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
